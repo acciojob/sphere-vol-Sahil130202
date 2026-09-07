17240c61c88e6f7a4e678640679858c42816b3c4
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-07 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.getElementById("radius");
+    let volume = document.getElementById("volume");
+
+	let r = Number(radius.value)
+
+	if(r<0 || isNaN(r)){
+		volume.value="NaN"
+		return false
+	}
+  let result = (4 / 3) * Math.PI * r * r * r
+	volume.value = result.toFixed(4)
+	return false
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
